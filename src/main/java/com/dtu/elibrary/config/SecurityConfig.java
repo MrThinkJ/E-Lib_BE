a7e64858ca0ae21611766c927d938a287345d1f4
MrThinkJ <ledinhthinh.ws@gmail.com>
2023-12-02 21:15:59 +0700

    fix security

--- src/main/java/com/dtu/elibrary/config/SecurityConfig.java
+++ src/main/java/com/dtu/elibrary/config/SecurityConfig.java
@@ -46,6 +46,8 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/author/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/publisher/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/book/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/book/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
